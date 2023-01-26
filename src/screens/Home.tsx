be3813ea795a97c618cad8fe1dcd073dd361da5f
MannySauce <33158848+MannySauce@users.noreply.github.com>
2023-01-26 16:38:06 -0700

    initial project

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -5,7 +5,7 @@ export default function Home({navigation}) {
   return (
     <View>
       <Text>Home</Text>
-      <Button title='Save Reminder' onPress={()=>navigation.navigate('RemindList')}/>
+      <Button title='Save Reminder' onPress={()=>navigation.navigate('RemindList', {id:'123', img: "lolsito.png", descr: "funky reminder", time: 23})}/>
     </View>
   )
 }
--- src/screens/RemindList.tsx
+++ src/screens/RemindList.tsx
@@ -1,10 +1,13 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 
-export default function RemindList() {
+export default function RemindList({route, navigation}) {
+    const {id, img, descr, time} = route.params;
+    console.log("route.params")
   return (
     <View>
       <Text>RemindList</Text>
+      <Text>{id}</Text>
     </View>
   )
 }
